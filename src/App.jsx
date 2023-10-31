341d1f185e23c05484f72fef8962287ed6736acd
agustintarancon <agustinbuf@gmail.com>
2023-10-30 22:38:20 -0300

    Implementacion de proyectos

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Navbar from "./components/navbar/Navbar"
 import Home from "../pages/home/Home"
 import Footer from "./components/footer/Footer"
-import "./App.css"
 
 function App() {
 
