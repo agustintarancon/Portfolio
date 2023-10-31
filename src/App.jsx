cbfaf7c50d7b70edbef45232ce01863cbb3d45bc
agustintarancon <agustinbuf@gmail.com>
2023-10-30 21:28:26 -0300

    Estructura base

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Navbar from "./components/navbar/Navbar"
+import Home from "../pages/home/Home"
+import Footer from "./components/footer/Footer"
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Navbar/>
+      <Routes>
+      <Route path="/" Component={Home}/>
+      </Routes>
+      <Footer/>
+    </BrowserRouter>
+  )
+}
+
+export default App
