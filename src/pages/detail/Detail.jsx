185c281c8e79ffb0fdb0d9c96ca1b2804f72aefb
agustintarancon <agustinbuf@gmail.com>
2025-01-14 17:32:41 -0300

    fix tamaños

--- src/pages/detail/Detail.jsx
+++ src/pages/detail/Detail.jsx
@@ -28,6 +28,7 @@ const Detail = () => {
             <img src={project.imgSource} className={img} alt={project.text1} />
           </Link>
         </div>
+        
         <div className={`col-12 col-md-12 col-lg-6 col-xl-6 ${descDiv}`}>
           <h2 className={title}>{project.text1}</h2>
           <p className={desc1}>{project.desc}</p>
