49432338d2411c719a3d537231e22803e1024998
agustintarancon <agustinbuf@gmail.com>
2025-01-14 15:22:51 -0300

    fix navbar y agrego animacion inferior

--- src/components/general/navbar/Navbar.jsx
+++ src/components/general/navbar/Navbar.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import { customLink, customNavbar, logoClass, customToggler } from "./navbar.module.css"
-import logo from "../../../assets/Logo.png"
+import { customLink, customNavbar, customToggler } from "./navbar.module.css"
 import { useLocation } from 'react-router-dom'
 import button from "../../../assets/buttonBurger.png"
 
@@ -10,9 +9,9 @@ const Navbar = () => {
 
   return (
     <>
-      <nav className={`navbar navbar-expand-lg p-3 px-5 position-fixed w-100 ${customNavbar}`}>
+      <nav className={`navbar navbar-expand-lg px-5 position-fixed w-100 ${customNavbar}`}>
         <a href="/" className="navbar-brand text-white" >
-          <img src={logo} alt="logo" className={logoClass} />
+          {/* <img src={logo} alt="logo" className={logoClass} /> */}
         </a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
           <img src={button} alt="Boton hamburguesa" className={customToggler}/>
@@ -20,17 +19,17 @@ const Navbar = () => {
         <div className="collapse navbar-collapse" id="navbar">
           <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
 
-            <li className="nav-item">
+            <li className="nav-item mb-3 mb-lg-0">
               <a href="/" className={`${customLink} text-decoration-none `} aria-current="page">INICIO</a>
             </li>
 
             {path === "/" && (
               <>
-                <li className="nav-item">
+                <li className="nav-item mb-3 mb-lg-0">
                   <a href="#aboutMe" className={`${customLink} text-decoration-none `} aria-current="page">SOBRE MI</a>
                 </li>
 
-                <li className="nav-item">
+                <li className="nav-item mb-3 mb-lg-0">
                   <a href="#projects" className={`${customLink} text-decoration-none `}>PROYECTOS</a>
                 </li>
               </>
